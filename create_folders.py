2a093dcc1cb80d568383df4fabe57028ec3fc389
Filipe Reis <filipe.breis@gmail.com>
2019-08-03 11:25:22 -0300

    Handles the complete process

--- create_folders.py
+++ create_folders.py
@@ -10,12 +10,22 @@ def create_folder(file, file_type=".mp4"):
     path = file.replace(file_type, "")
     if not os.path.exists(path):
         os.makedirs(path)
+    return path
+
+def move_file(file, new_path):
+    _, file_name = os.path.split(file)
+
+    new_path = new_path +"/"+ file_name
+    os.rename(file, new_path)
+
 if __name__ == "__main__":
     path = ""
     files = get_files(path)
     print(files)
     for file in files:
-        create_folder(file)
+        new_path = create_folder(file)
+        move_file(file, new_path)
+
 
 
 
